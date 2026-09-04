8e9f627297e8561674183fce90e4531fdaf7adc2
tilscn <you@example.com>
2026-09-04 22:58:42 +0800

    feat:element plus

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+<el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
 </template>
 
 <style scoped></style>
